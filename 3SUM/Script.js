68844a889fcf49d7aa37025b1f07ab2f83c915ea
Ayabonga Qwabi <ayabonga@projectcodex.co>
2015-03-16 06:15:56 +0200

    Initial site Design by webmatrix

---
+++ 3SUM/Script.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function () {
+
+    $('.social1').css("background-color","blue");
+    $('.social2').
+    $('.social3').hide();
+
+    $('#Mluja').mouseenter(function () {
+        $('.social1').hide();
+    });
+});
